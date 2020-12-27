975bbe0333fa6f1ac4e2074c5b494bb0d3b3e3e9
Moshe Dicker <dickermoshe@gmail.com>
2020-12-27 16:28:33 +0000

    initial

--- main.py
+++ main.py
@@ -9,20 +9,20 @@ class mvids():
     
     def __init__(self):
 
-        self.test = True
+        self.test = False
         ##URLs for indexing Movies and TV##
         self.indexMovieURL = 'https://mobilevids.org/webapi/videos/movies.php'
-        self.ShowScreenURL='https://mobilevids.org/webapi/videos/get_season.php'
+        self.MainShowURL='https://mobilevids.org/webapi/videos/get_season.php'
         self.EpisodeScreenURL='https://mobilevids.org/webapi/videos/get_episodes.php'
         self.MovieScreenURL = 'https://mobilevids.org/webapi/videos/get_video.php'
         self.indexTvURL = 'https://mobilevids.org/webapi/videos/tvshows.php'
         self.loginURL = 'https://mobilevids.org/webapi/user/login.php'
         self.username = 'mickrich345@gmail.com'
         self.code = "7897412563"
-        self.masterkeys = {'login':['id','auth_token'],'pagesamount':['total_pages'],'totalindex':['items'],'addMovie':['src_free_sd','src_vip_sd','src_vip_hd','src_vip_hd_1080p']}
+        self.masterkeys = {'login':['id','auth_token'],'pagesamount':['total_pages'],'totalindex':['items'],'addMovie':['src_free_sd','src_vip_sd','src_vip_hd','src_vip_hd_1080p'],'appendShow':['season_list'],'appendEpisodes':['episodes']}
         self.opt=['src_free_sd','src_vip_sd','src_vip_hd','src_vip_hd_1080p']
         ##Set minimum execution times for different requests ##
-        self.wait = {'login':1,'pagesamount':1,'totalindex':1,'moviepage':1,'showpage':1}
+        self.wait = {'login':1,'pagesamount':1,'totalindex':1,'moviepage':1,'showpage':1,'addMovie':1,'appendEpisodes':1}
         
         ##Set active directery to location of script ##
         self._setCWD()
@@ -104,7 +104,7 @@ class mvids():
 
         return int(response['total_pages'])
     def totalindex(self,mot):#Get all TV shows And Movies
-        index = []
+        index = {}
         itera = range(1,self.pagenum[mot]+1) if not self.test else range(1,3)
         for i in itera:
             params=[['user_id',self.user_id],['token',self.token],['p',str(i)]]
@@ -134,88 +134,65 @@ class mvids():
         response = self.respCall(url,'addMovie','get',params = params)
         x = self.respParse(response,'addMovie')
         self.appendMovieIndex(id,x)
-
-    
-    
-    def appendEpisodes(self,id,season,info):
-        found = False
-
-        if 'not found' in str(info.get('reason')):
-            self.tvindex = self.totalindex('t')
-            return False
+    def appendEpisodes(self,id,season,info):#Get URL for entire season
         while True:
             amount = len(self.tvindex)
             for i in range(amount):
-                if str(self.tvindex[i]['ID']) == str(id):
-                    found = True
-                    if self.tvindex[i].get('season_list') == None:
-                        self.tvindex[i]['season_list'] = []
-
-                    self.tvindex[i]['season_list'].append({season:{q['episode']:[q.get('src_free_sd'),q.get('src_vip_sd'),q.get('src_vip_hd'),q.get('src_vip_hd_1080p')] for q in info.get('episodes')}})
-                    break
-            if found == True:
-                break
-            else:
-                self.tvindex = self.totalindex('t')
-                continue
-        return True
-
-    def parseShow(self,id,info):
-        found = False
-
-        if 'not found' in str(info.get('reason')):
-            self.tvindex = self.totalindex('t')
-            return False
-        while True:
-            amount = len(self.tvindex)
-            for i in range(amount):
-                if str(self.tvindex[i]['ID']) == str(id):
-                    found = True
-                    return info.get('season_list').keys()
-                    break
-            if found == True:
-                break
-            else:
-                self.tvindex = self.totalindex('t')
-                continue
-        return True
-    
-    def appendShow(self,id):
-        
-
+                if str(self.movieindex[i]['ID']) == str(id) and season in self.movieindex[i]['season_list']:
+                    url = self.EpisodeScreenURL
+                    params =[['show_id', id],['season',str(i)],['user_id',self.user_id],['token',self.token]]
+                    response = self.respCall(url,'appendEpisodes','get',params = params)
+                    x = self.respParse(response,'appendEpisodes')
+                    self.movieindex[i][season] = {q['episode']:[q.get('src_free_sd'),q.get('src_vip_sd'),q.get('src_vip_hd'),q.get('src_vip_hd_1080p')] for q in x['episodes']}
+                    return
+            print('Not found Refreshing List...')
+            self.movieindex = self.totalindex('t')
+    def appendShow(self,id):# Get Show Meta Data
         params = [['show_id', id],['user_id',self.user_id],['token',self.token]]
-        url = self.ShowScreenURL
-        response = self.respCall(url,'moviepage','get',params = params)
-        z= self.parseShow(id,response)
+        url = self.MainShowURL
+        response = self.respCall(url,'appendShow','get',params = params)
+        parsed = self.respParse(response,'appendShow')
 
-        url = self.EpisodeScreenURL
-        for i in list(z) :       
-            twoX = False
-            while True:
-                print('finish deson',i)
-                params =[['show_id', id],['season',str(i)],['user_id',self.user_id],['token',self.token]]
-                response = self.respCall(url,'moviepage','get',params = params)
-                x = self.appendEpisodes(id,i,response)
-                if x:
-                    break
-                else:
-                    print('Clould not find this season. Trying again.')
-                    if twoX == True:
-                        print('Clould not find this season. Moving on to next one')
-                        break
-                    twoX = True
-                    continue
+        while True:
+            amount = len(self.tvindex)
+            for i in range(amount):
+                if str(self.movieindex[i]['ID']) == str(id):
+                    self.movieindex[i]['season_list'] = list(parsed.keys())
+                    return
+            print('Not found Refreshing List...')
+            self.movieindex = self.totalindex('t')
+    def printTV(self):#Print all TV Shows
+        for i in self.tvindex:
+            print(i['TITLE'],' : ',i['ID'])
 
+    def printMovie(self):#Print all Movies
+        for i in self.movieindex:
+            print(i['TITLE'],' : ',i['ID'])
+    def tvIDCheck(self,id):#Check if TV Show ID is in system
+        for i in self.tvindex:
+            if str(i['ID']) == str(id):
+                return True
+        return False
+    
+    
+    def movieIDCheck(self,id):#Check if Movie ID is in system
+        for i in self.movieindex:
+            if str(i['ID']) == str(id):
+                return True
+        return False
 
-
+    def printMovieURL(self,id):
+        if movieIDCheck(id):
+            print([self.movieindex])
 
 x = mvids()
-if x.test:
-    print(x.tvindex)
-z= str(input('enter number of tv show'))
-print(z,type(z))
-x.appendShow(z)
-print(x.tvindex)
+x.printMovie()
+x.printTV()
+
+        
+
+
+
 
 
 #response = requests.get('https://mobilevids.org/webapi/videos/tvshows.php', params=params)
