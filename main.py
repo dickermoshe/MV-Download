803a47f9de64068338f98e03a14ba43a156575af
Moshe Dicker <dickermoshe@gmail.com>
2020-12-27 14:16:53 +0000

    Clean Movies. Need to clean TV Shows.

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ class mvids():
         self.loginURL = 'https://mobilevids.org/webapi/user/login.php'
         self.username = 'mickrich345@gmail.com'
         self.code = "7897412563"
-        
+        self.masterkeys = {'login':['id','auth_token'],'pagesamount':['total_pages'],'totalindex':['items'],'addMovie':['src_free_sd','src_vip_sd','src_vip_hd','src_vip_hd_1080p']}
+        self.opt=['src_free_sd','src_vip_sd','src_vip_hd','src_vip_hd_1080p']
         ##Set minimum execution times for different requests ##
         self.wait = {'login':1,'pagesamount':1,'totalindex':1,'moviepage':1,'showpage':1}
         
@@ -34,68 +35,108 @@ class mvids():
         self.movieindex, self.tvindex = self.totalindex('m'),self.totalindex('t')
         print(len(self.movieindex),len(self.tvindex))
 
-    def respCall(self,url,master,method,params = dict(a=None),data = dict(a=None)):
-        threeX = 0
-        x = 0
-        while x == 0:
-            x = 1
-            st = time.time()
-            if method == 'get':
+    def respCall(self,url,master,method,params = dict(a=None),data = dict(a=None)):#Call and ensure proper format
+        failed = 1
+        while True:
+            st = time.time()# Get Start Time
+            
+            if method == 'get': #Check Method
                 response = self.s.get(url,params=params,data = data)
             elif method == 'post':
                 response = self.s.post(url,params=params,data = data)
             else:
                 print("Must be post OR get.")
                 sys.exit()
-            end = time.time()
             
-            if end - st < self.wait[master]:
+            end = time.time()# Get End Time
+            
+            if end - st < self.wait[master]:# Wait Predetermined Time
                 time.sleep(self.wait[master]-(end - st))
-
-            try:
+            
+            try:# Parse as JSON
                 y = response.json()
-            except:
-                if threeX != 3:
-                    threeX +=1
-                    x = 0
-                    print(response.text,"\nCould not JSON.Try Again in 15 sec")
-                    time.sleep(15)
+            
+            except:#If fail try agin 3 time
+                if failed < 4:
+                    print(response.text,"\nCould not JSON. Sleeping ",5*failed,' seconds.')
+                    time.sleep(5*failed)
+                    failed +=1
                     continue
                 else:
                     print(response.text,"\nCould not JSON. Exiting")
                     sys.exit()
             if 'session missmatch' in str(y.get('reason')):
                 self.login()
-                x = 0
                 continue
-        return y
-
-    def login(self):
-
+            z = self.respParse(y,master)
+            if z == False:
+                print('Could not find ',master, ' keys in ',y,'\nTrying Again.' )
+                time.sleep(5)
+                continue
+            break
+        return z
+    def respParse(self,data,master):#Ensure corect return
+        parseddata = {}
+        for i in self.masterkeys[master]:
+            if data.get(i) == None and i not in self.opt:
+                return False
+            else:
+                parseddata[i] = data.get(i)
+        return parseddata
+    def login(self):#Login
 
         data = {'data': '{"Name":"'+self.username+'","Password":"'+self.code+'"}'}
         response = self.respCall(self.loginURL,'login','post',data = data)
 
         self.user_id = response['id']
         self.token = response['auth_token']
-    def appendMovie(self,id,info):
-        found = False
-        if 'not found' in str(info.get('reason')):
-            self.movieindex = self.totalindex('m')
-            return False
+    def _setCWD(self):#Set CWD to Script Location
+        os.chdir(os.path.split(os.path.abspath(os.path.realpath(sys.argv[0])))[0])
+    def pagesamount(self,mot):#Get Amount OF pages
+        params=[['user_id',self.user_id],['token',self.token]]
+        if mot == 'm':
+            response = self.respCall(self.indexMovieURL,'pagesamount','get',params = params)
+        elif mot == 't':
+            response = self.respCall(self.indexTvURL,'pagesamount','get',params = params)
+        else:
+            print(mot,"\nMust be 'm'ovie OR 't'v show.")
+            sys.exit()
+
+        return int(response['total_pages'])
+    def totalindex(self,mot):#Get all TV shows And Movies
+        index = []
+        itera = range(1,self.pagenum[mot]+1) if not self.test else range(1,3)
+        for i in itera:
+            params=[['user_id',self.user_id],['token',self.token],['p',str(i)]]
+            if mot == 'm':
+                response = self.respCall(self.indexMovieURL,'totalindex','get',params = params)
+            elif mot == 't':
+                response = self.respCall(self.indexTvURL,'totalindex','get',params = params)
+            else:
+                print(mot,"\nMust be 'm'ovie OR 't'v show.")
+                sys.exit()
+
+            index = index + [{'TITLE':html.unescape(z['title']),'ID':z['id']} for z in response['items']]
+
+        return index   
+    def appendMovieIndex(self,id,info):#Add URLs to Movie
         while True:
             amount = len(self.movieindex)
             for i in range(amount):
                 if str(self.movieindex[i]['ID']) == str(id):
-                    found = True
                     self.movieindex[i].update(info)
-                    break
-            if found == True:
-                break
-            else:
-                self.movieindex = self.totalindex('m')
-                continue
-        return True
+                    return
+            print('Not found Refreshing List...')
+            self.movieindex = self.totalindex('m')
+    def addMovie(self,id):#Get Movie URLs and Update
+        params = [['id', id],['user_id',self.user_id],['token',self.token]]
+        url = self.MovieScreenURL
+        response = self.respCall(url,'addMovie','get',params = params)
+        x = self.respParse(response,'addMovie')
+        self.appendMovieIndex(id,x)
+
+    
+    
     def appendEpisodes(self,id,season,info):
         found = False
 
@@ -165,59 +206,8 @@ class mvids():
                     twoX = True
                     continue
 
-    def appendMovie(self,id):
-        twoX = False
-        while True :
-            params = [['id', id],['user_id',self.user_id],['token',self.token]]
-            url = self.MovieScreenURL
-            response = self.respCall(url,'moviepage','get',params = params)
-            x = self.appendMovie(id,response)
-            if x:
-                break
-            else:
-                print('Clould not find. Trying again.')
-                if twoX == True:
-                    print('Could Not Find')
-                    sys.exit()
-                twoX = True
-                continue   
 
-    def _setCWD(self):
-        os.chdir(os.path.split(os.path.abspath(os.path.realpath(sys.argv[0])))[0])
-    
-    def pagesamount(self,mot):
-        params=[['user_id',self.user_id],['token',self.token]]
-        if mot == 'm':
-            response = self.respCall(self.indexMovieURL,'pagesamount','get',params = params)
-        elif mot == 't':
-            response = self.respCall(self.indexTvURL,'pagesamount','get',params = params)
-        else:
-            print(mot,"\nMust be 'm'ovie OR 't'v show.")
-            sys.exit()
-        if 'total_pages' in response:
-            return int(response['total_pages'])
-        else:
-            print(response,"\nCould not get Page Amount.")
-            sys.exit()
-    
-    def totalindex(self,mot):
-        index = []
-        itera = range(1,self.pagenum[mot]+1) if not self.test else range(1,3)
-        for i in itera:
-            params=[['user_id',self.user_id],['token',self.token],['p',str(i)]]
-            if mot == 'm':
-                response = self.respCall(self.indexMovieURL,'totalindex','get',params = params)
-            elif mot == 't':
-                response = self.respCall(self.indexTvURL,'totalindex','get',params = params)
-            else:
-                print(mot,"\nMust be 'm'ovie OR 't'v show.")
-                sys.exit()
-            if 'items' in response:
-                index = index + [{'TITLE':html.unescape(z['title']),'ID':z['id']} for z in response['items']]
-            else:
-                print(response,"\nPage Details not found.")
-                sys.exit()
-        return index
+
 
 x = mvids()
 if x.test:
