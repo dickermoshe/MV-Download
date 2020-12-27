d55dece64da7712926185382a64d1291e64d6b25
Moshe Dicker <dickermoshe@gmail.com>
2020-12-27 13:04:25 +0000

    URL Puller

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class mvids():
         ##URLs for indexing Movies and TV##
         self.indexMovieURL = 'https://mobilevids.org/webapi/videos/movies.php'
         self.ShowScreenURL='https://mobilevids.org/webapi/videos/get_season.php'
+        self.EpisodeScreenURL='https://mobilevids.org/webapi/videos/get_episodes.php'
         self.MovieScreenURL = 'https://mobilevids.org/webapi/videos/get_video.php'
         self.indexTvURL = 'https://mobilevids.org/webapi/videos/tvshows.php'
         self.loginURL = 'https://mobilevids.org/webapi/user/login.php'
@@ -20,19 +21,21 @@ class mvids():
         self.code = "7897412563"
         
         ##Set minimum execution times for different requests ##
-        self.wait = {'login':1,'pagesamount':1,'totalindex':0,'moviepage':1,'showpage':1}
+        self.wait = {'login':1,'pagesamount':1,'totalindex':1,'moviepage':1,'showpage':1}
         
         ##Set active directery to location of script ##
         self._setCWD()
         self.s = requests.session()
         ##Set Variables self.user_id and self.token ## 
         self.login()
+
         ## Return length of pages
         self.pagenum = {'m':self.pagesamount('m'),'t':self.pagesamount('t')}
         self.movieindex, self.tvindex = self.totalindex('m'),self.totalindex('t')
         print(len(self.movieindex),len(self.tvindex))
 
     def respCall(self,url,master,method,params = dict(a=None),data = dict(a=None)):
+        threeX = 0
         x = 0
         while x == 0:
             x = 1
@@ -52,8 +55,15 @@ class mvids():
             try:
                 y = response.json()
             except:
-                print(response.text,"\nCould not JSON.")
-                sys.exit()
+                if threeX != 3:
+                    threeX +=1
+                    x = 0
+                    print(response.text,"\nCould not JSON.Try Again in 15 sec")
+                    time.sleep(15)
+                    continue
+                else:
+                    print(response.text,"\nCould not JSON. Exiting")
+                    sys.exit()
             if 'session missmatch' in str(y.get('reason')):
                 self.login()
                 x = 0
@@ -86,7 +96,7 @@ class mvids():
                 self.movieindex = self.totalindex('m')
                 continue
         return True
-    def appendShow(self,id,info):
+    def appendEpisodes(self,id,season,info):
         found = False
 
         if 'not found' in str(info.get('reason')):
@@ -97,7 +107,10 @@ class mvids():
             for i in range(amount):
                 if str(self.tvindex[i]['ID']) == str(id):
                     found = True
-                    self.tvindex[i]['season_list'] = {info.get}
+                    if self.tvindex[i].get('season_list') == None:
+                        self.tvindex[i]['season_list'] = []
+
+                    self.tvindex[i]['season_list'].append({season:{q['episode']:[q.get('src_free_sd'),q.get('src_vip_sd'),q.get('src_vip_hd'),q.get('src_vip_hd_1080p')] for q in info.get('episodes')}})
                     break
             if found == True:
                 break
@@ -106,19 +119,59 @@ class mvids():
                 continue
         return True
 
-            
+    def parseShow(self,id,info):
+        found = False
 
+        if 'not found' in str(info.get('reason')):
+            self.tvindex = self.totalindex('t')
+            return False
+        while True:
+            amount = len(self.tvindex)
+            for i in range(amount):
+                if str(self.tvindex[i]['ID']) == str(id):
+                    found = True
+                    return info.get('season_list').keys()
+                    break
+            if found == True:
+                break
+            else:
+                self.tvindex = self.totalindex('t')
+                continue
+        return True
+    
+    def appendShow(self,id):
+        
 
-    def appendpage(self,mot,id):
+        params = [['show_id', id],['user_id',self.user_id],['token',self.token]]
+        url = self.ShowScreenURL
+        response = self.respCall(url,'moviepage','get',params = params)
+        z= self.parseShow(id,response)
+
+        url = self.EpisodeScreenURL
+        for i in list(z) :       
+            twoX = False
+            while True:
+                print('finish deson',i)
+                params =[['show_id', id],['season',str(i)],['user_id',self.user_id],['token',self.token]]
+                response = self.respCall(url,'moviepage','get',params = params)
+                x = self.appendEpisodes(id,i,response)
+                if x:
+                    break
+                else:
+                    print('Clould not find this season. Trying again.')
+                    if twoX == True:
+                        print('Clould not find this season. Moving on to next one')
+                        break
+                    twoX = True
+                    continue
+
+    def appendMovie(self,id):
         twoX = False
         while True :
-            params = [['id', id],['user_id',self.user_id],['token',self.token]] if mot =='m' else [['show_id', id],['user_id',self.user_id],['token',self.token]]
-            url = self.MovieScreenURL if mot =='m' else self.ShowScreenURL
+            params = [['id', id],['user_id',self.user_id],['token',self.token]]
+            url = self.MovieScreenURL
             response = self.respCall(url,'moviepage','get',params = params)
-            if mot == 'm':
-                x = self.appendMovie(id,response)
-            if mot == 't':
-                x = self.appendShow(id,response)
+            x = self.appendMovie(id,response)
             if x:
                 break
             else:
@@ -165,12 +218,13 @@ class mvids():
                 print(response,"\nPage Details not found.")
                 sys.exit()
         return index
+
 x = mvids()
 if x.test:
     print(x.tvindex)
 z= str(input('enter number of tv show'))
 print(z,type(z))
-x.appendpage('t',z)
+x.appendShow(z)
 print(x.tvindex)
 
 
